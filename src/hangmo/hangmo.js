2a45dcb255639199f013f390ee824641de9694bf
Caio Patriota <caiopatriotabarros@gmail.com>
2024-05-17 21:40:30 -0300

    keyboard

--- src/hangmo/hangmo.js
+++ src/hangmo/hangmo.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function HangmanGame() {
   const secretWord = 'apple'; // You can change this to be any word you want
   const [guesses, setGuesses] = useState([]);
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 
   const handleGuess = (letter) => {
     setGuesses([...guesses, letter]);
@@ -18,15 +18,18 @@ function HangmanGame() {
     <div>
       <h1>Hangman Game -- HANGMO</h1>
       <p>{displayWord}</p>
-      {alphabet.map((letter) => (
-        <button
-          key={letter}
-          onClick={() => handleGuess(letter)}
-          disabled={guesses.includes(letter)}
-        >
-          {letter}
-        </button>
-      ))}
+      <div id="alphabet">
+        {alphabet.map((letter) => (
+          <button
+            key={letter}
+            className="letter"
+            onClick={() => handleGuess(letter)}
+            disabled={guesses.includes(letter)}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
